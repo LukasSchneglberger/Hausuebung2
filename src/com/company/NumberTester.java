bebfff2c97c36e5a0160d80441bf137e14b1d069
Lukas Schneglberger <l.schneglberger@gmail.com>
2022-09-23 12:42:10 +0200

    Worked at NumberTester.java

--- src/com/company/NumberTester.java
+++ src/com/company/NumberTester.java
@@ -1,10 +1,35 @@
 package com.company;
 
-public class NumberTester{
-    // ...
-    public NumberTester ( String fileName ) {
-        // TODO
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class NumberTester{
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    private String fileName;
+    public NumberTester (String fileName ) {
+       this.fileName = fileName;
+       oddTester = (n) -> (n % 2) == 0;
+       primeTester = (n) -> {
+           if (n <= 1)
+               return false;
+
+           for (int i = 2; i < n; i++)
+               if (n % i == 0)
+                   return false;
+
+           return true;
+       };
+        palindromeTester = (n) -> {
+
+            String reverse = new StringBuffer(n + "").reverse().toString();
+
+
+            return (n + "").equals(reverse);
+        };
     }
     public void setOddEvenTester (NumberTest oddTester) {
         this.oddTester = oddTester ;
@@ -17,6 +42,16 @@ public class NumberTester{
         this.palindromeTester = palindromeTester ;
 }
     public void testFile ( ) {
-     // TODO
-}
+
+        try {
+            Scanner s = new Scanner(new File(fileName));
+            while (s.hasNextLine()){
+                
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
