4d1e521c67fd55ec1d16f22e980a8b9a7be2271d
Lukas Schneglberger <l.schneglberger@gmail.com>
2022-09-23 12:06:23 +0200

    Beispiel 2 working

---
+++ src/com/company/HalloJavamitForEach.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Foo");
+        list.add("Bar");
+        list.add("FooBar");
+        list.add("BarFoo");
+
+        for(String s : list){
+            System.out.println(s);
+        }
+
+        list.forEach((s) -> System.out.println(s));
+        l
+
+
+
+    }
+}
